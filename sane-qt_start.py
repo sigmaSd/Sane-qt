afb4e9e3d2a16dc1ab2e369108c9af1c44223bef
sigmaSd <bedisnbiba@gmail.com>
2018-01-04 19:42:43 +0100

    Upgraded to PyQt5 , Added exceptions handling

--- sane-qt_start.py
+++ sane-qt_start.py
@@ -1,23 +1,29 @@
 import sys
-from PyQt4 import QtCore, QtGui
-from saneqt import Ui_StackedWidget
+from PyQt5 import QtCore, QtGui, QtWidgets
+from interface import Ui_StackedWidget
 from subprocess import getoutput
 from subprocess import run
+from pathlib import  Path
 
-class StartQT4(QtGui.QStackedWidget):
+class StartQT5(QtWidgets.QStackedWidget):
 
-    global directory
+    global  directory
+    global  name_changed
 
     def switch_window(self):
-        self.ui.page.hide()
-        self.ui.page1.show()
+        if self.ui.listWidget.count() > 0 :
+            self.ui.page.hide()
+            self.ui.page_2.show()
+
     def back_to_main(self):
-        self.ui.page1.hide()
+        self.ui.page_2.hide()
         self.ui.page.show()
 
     def scanners_list(self):
+
         final_list = []
         raw_output = getoutput("scanimage -L")
+        #raw_output ="azeae"
         lines_list = raw_output.split('\n')
         for line in lines_list:
             line = line.lstrip("device")
@@ -25,45 +31,71 @@ class StartQT4(QtGui.QStackedWidget):
                 if line[i] == "'":
                     final_list.append(line[2:i])
                     break
+
+        if final_list == [] :
+            self.ui.label_2.show()
+            self.ui.listWidget.hide()
         for i in final_list:
             self.ui.listWidget.addItem(i)
 
+        self.ui.listWidget.focusNextChild()
+    def check_again(self):
+        self.ui.listWidget.clear()
+        self.ui.listWidget.show()
+        self.ui.label_2.hide()
+        self.scanners_list()
+
 
     def dir (self) :
-        self.directory = str(QtGui.QFileDialog.getExistingDirectory(self, "Select Directory"))
-        self.ui.pushButton_3.setText("Ok")
+        self.directory = str(QtWidgets.QFileDialog.getExistingDirectory(self, "Select Directory"))
+        self.ui.pushButton_5.setText("Ok")
+        self.ui.label.hide()
 
 
     def change_button_name(self):
-        text, ok = QtGui.QInputDialog.getText(self, 'Choose name', 'name:')
+        text, ok = QtWidgets.QInputDialog.getText(self, 'Choose name', 'name:')
         if ok:
-            self.ui.pushButton_4.setText(str(text))
+            self.ui.pushButton_6.setText(str(text))
+            self.name_changed=True
 
     def scan(self):
-        device = self.ui.listWidget.currentItem().text()
-        output = self.ui.pushButton_4.text().__str__()
-        dir = self.directory + "/" + output
 
-
-        run([" scanimage -d " + device + ">" + " " + dir ], shell=True)
-        self.ui.graphicsView.setPixmap(QtGui.QPixmap(dir).scaled(QtCore.QSize(256,271)))
+        if self.name_changed==False :
+            self.ui.label.setText("Name your file !")
+            self.ui.label.show()
+        if self.directory =="none" :
+            self.ui.label.setText("Choose a directory first !")
+        if self.name_changed and self.directory !="none" :
+            device = self.ui.listWidget.currentItem().text()
+            output = self.ui.pushButton_6.text().__str__().replace(" ","")
+            dir = self.directory + "/" + output
+            run([" scanimage -d " + device + ">" + " " + dir ], shell=True)
+            if Path(dir).is_file():
+                self.ui.label.setPixmap(QtGui.QPixmap(dir).scaled(QtCore.QSize(256,271)))
+                self.ui.label.show()
+                self.ui.pushButton_6.setText("choose name")
+                self.name_changed=False
 
     def __init__(self, parent=None):
-        QtGui.QWidget.__init__(self, parent)
+        super(StartQT5,self).__init__()
         self.ui = Ui_StackedWidget()
         self.ui.setupUi(self)
+        self.directory="none"
+        self.name_changed = False
+        self.ui.label_2.hide()
         self.scanners_list()
+        self.ui.page_2.hide()
         self.ui.page.show()
-        self.ui.page1.hide()
+        self.ui.pushButton.clicked.connect(self.switch_window)
+        self.ui.pushButton_5.clicked.connect(self.dir)
+        self.ui.pushButton_6.clicked.connect(self.change_button_name)
+        self.ui.pushButton_3.clicked.connect(self.scan)
+        self.ui.pushButton_4.clicked.connect(self.back_to_main)
+        self.ui.pushButton_2.clicked.connect(self.check_again)
 
-        QtCore.QObject.connect(self.ui.pushButton, QtCore.SIGNAL("clicked()"),self.switch_window)
-        QtCore.QObject.connect(self.ui.pushButton_3, QtCore.SIGNAL("clicked()"), self.dir)
-        QtCore.QObject.connect(self.ui.pushButton_4, QtCore.SIGNAL("clicked()"), self.change_button_name)
-        QtCore.QObject.connect(self.ui.pushButton_2, QtCore.SIGNAL("clicked()"), self.scan)
-        QtCore.QObject.connect(self.ui.pushButton_5, QtCore.SIGNAL("clicked()"), self.back_to_main)
 
 if __name__ == "__main__":
-    app = QtGui.QApplication(sys.argv)
-    myapp = StartQT4()
+    app = QtWidgets.QApplication(sys.argv)
+    myapp = StartQT5()
     myapp.show()
     sys.exit(app.exec_())
